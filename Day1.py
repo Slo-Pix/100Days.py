02a53a10514dd5a2f35e8f999b281fc5979c87a4
Slo-Pix <slopixelz@gmail.com>
2025-08-17 04:25:48 +0530

    band_name_generator

---
+++ Day1.py
@@ -0,0 +1,11 @@
+# Band name generator
+# What I learn - Print, Input, Comments, Debugging, Variables, String Manipulation
+
+print("\n-------------------------")
+print("[+] Band Name Generator")
+print("-------------------------")
+
+x = input("Where were you born?\n")
+y = input("What is your favorite fictional character?\n")
+z = x + " " + y
+print("Your band name is - " + z)
